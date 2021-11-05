928f66f1ac5a1bc7b0cf9a6a9dad6e817f909763
ALFRED <alfredbate@yahoo.co.uk>
2021-11-04 21:47:31 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to our site!</h1>
+     
+
+    </div>
+  );
+}
+
+export default App;
